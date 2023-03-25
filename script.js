a022102201473bc6e225aabfd95650e6d24b7e86
cpt_n3m0 <wijayaandre769@gmail.com>
2023-03-26 00:40:45 +0700

    Calculator

---
+++ script.js
@@ -0,0 +1,104 @@
+// Cache DOM elements
+const screen = document.querySelector('.screen');
+const buttons = document.querySelector('.cal-buttons');
+
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+let shouldUpdateScreen = true;
+
+// Button click event listener
+buttons.addEventListener('click', function(event) {
+    const target = event.target;
+
+    // Ignore clicks on non-button elements
+    if (!target.matches('button')) {
+        return;
+    }
+
+    const value = target.innerText;
+    buttonClick(value);
+});s
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    if (shouldUpdateScreen) {
+        screen.innerText = buffer;
+    }
+}
+
+function handleSymbol(symbol) {
+switch (symbol) {
+    case 'C':
+        buffer = '0';
+        runningTotal = 0;
+        shouldUpdateScreen = true;
+        break;
+    case '=':
+        if (previousOperator === null) {
+        return;
+        }
+        flushOperation(parseInt(buffer));
+        previousOperator = null;
+        buffer = runningTotal;
+        runningTotal = 0;
+        shouldUpdateScreen = true;
+        break;
+    case '←':
+        if (buffer.length === 1) {
+        buffer = '0';
+        } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+        }
+        shouldUpdateScreen = true;
+        break;
+    case '+':
+    case '−':
+    case '×':
+    case '÷':
+        handleMath(symbol);
+        shouldUpdateScreen = true;
+        break;
+    }
+}
+
+function handleMath(symbol) {
+  if (buffer === '0') {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === '+') {
+    runningTotal += intBuffer;
+  } else if (previousOperator === '−') {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === '×') {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === '÷') {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+  shouldUpdateScreen = true;
+}
